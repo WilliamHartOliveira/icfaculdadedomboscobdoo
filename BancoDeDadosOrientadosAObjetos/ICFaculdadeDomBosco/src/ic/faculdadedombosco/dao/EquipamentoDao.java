7bc8b2e81a64010f32e3fd72d3162d4da1e0e717
icfacdombosco2010 <icfacdombosco2010@12ca0d6e-1208-20e4-66e8-9baacc9190fc>
2010-09-01 05:45:11 +0000

    Design Patterns

---
+++ BancoDeDadosOrientadosAObjetos/ICFaculdadeDomBosco/src/ic/faculdadedombosco/dao/EquipamentoDao.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ic.faculdadedombosco.dao;
+
+import ic.faculdadedombosco.model.Equipamento;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Anderson
+ */
+public class EquipamentoDao {
+
+    public Equipamento incluir(Equipamento equipamento){
+
+        return equipamento;
+    }
+
+    public Equipamento atualizar(Equipamento equipamento){
+
+        return equipamento;
+    }
+
+    public Equipamento excluir(Equipamento equipamento){
+
+        return equipamento;
+    }
+
+    public Equipamento buscar(Equipamento equipamento){
+
+        return equipamento;
+    }
+
+    public List<Equipamento> listar(){
+
+        List<Equipamento> listaEquipamento = new ArrayList<Equipamento>();
+
+        return listaEquipamento;
+    }
+
+}
---
+++ BancoDeDadosOrientadosAObjetos/ICFaculdadeDomBosco/src/ic/faculdadedombosco/dao/PersistenciaDao.java
@@ -0,0 +1,86 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ic.faculdadedombosco.dao;
+
+import com.db4o.*;
+import com.db4o.ObjectSet;
+import ic.faculdadedombosco.model.Equipamento;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+
+/**
+ *
+ * @author Anderson
+ */
+public class PersistenciaDao {
+
+    static ObjectContainer db = null;
+
+    public void AbrirConexao(){
+       db = Db4o.openFile("facdombosco.dbo"); 
+       System.out.println("Conexão aberta...");
+    }
+
+    public  void FecharConexao(){
+        db.close();
+        try{
+            System.out.println("Conexão fechada...");
+        }
+        catch(Exception ex){
+            System.out.println("Erro em fechar conexão: "+ex);
+        }
+    }
+    public void Salvar(Equipamento equip){
+        try{
+            db.store(equip);            
+        }
+        catch(Exception ex){
+            System.out.println("Erro ao salvar o objeto!!!\n"+ex);
+            JOptionPane.showMessageDialog(null, "Erro ao salvar o objeto...\n"+ex, "Atenção!!!", JOptionPane.ERROR_MESSAGE);
+        }
+
+    }
+
+    public void alterar(){
+
+    }
+
+    public void Excluir(){
+
+    }
+
+    /*public static Equipamento Pesquisar(String ds_equipamento){
+
+        Query consulta = db.query();
+        consulta.constrain(Equipamento.class);
+        consulta.descend("ds_equipamento").constrain(ds_equipamento);
+
+        ObjectSet lista = consulta.execute();
+
+
+        if (lista.size() > 0) {
+            return (Equipamento) lista.toString();
+
+        } else {
+            return null;
+        }
+    }*/
+
+    public List get(Equipamento equipamento){
+        ObjectSet obj;
+        obj = db.get(equipamento);
+        System.out.println("Tamanho do resultado: "+obj.size());
+        
+        List<Equipamento> list = new ArrayList<Equipamento>();
+        
+        while(obj.hasNext()){
+            System.out.println(list.add((Equipamento)obj.next()));
+        }
+        return list;        
+    }
+}
