31f370f1f402a0a904f2f69d562e4d9b3578ca7b
icfacdombosco2010 <icfacdombosco2010@12ca0d6e-1208-20e4-66e8-9baacc9190fc>
2010-10-21 12:01:39 +0000

    Alterado por William.
    Detonando na programação!

--- BancoDeDadosOrientadosAObjetos/ICFaculdadeDomBosco/src/ic/faculdadedombosco/Conexao.java
+++ BancoDeDadosOrientadosAObjetos/ICFaculdadeDomBosco/src/ic/faculdadedombosco/Conexao.java
@@ -1,38 +1,60 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package ic.faculdadedombosco;
 
 import com.db4o.Db4o;
 import com.db4o.ObjectContainer;
 
-/**
- *
+/*
  * @author Anderson
+ * @author William
  */
 public class Conexao {
 
-    private static ObjectContainer db;
+    private static ObjectContainer oObjectContainer;
+    private static StringBuilder sb = new StringBuilder();
 
     public void abrirConexao(){
-        db = Db4o.openFile("facdombosco.dbo");
-        System.out.println("Conexão aberta...");
+        try {
+            oObjectContainer = Db4o.openFile("facdombosco.dbo");
+            sb.append("Conexão Realizada com Sucesso.");
+        }catch(Exception ex){
+            sb.append("Erro ao abrir conexão: "+ex);
+        }
+        finally{
+
+            if (sb.length() > 0) {
+                try {
+                    throw new Exception(sb.toString());
+                }
+                catch (Exception ex) {
+                    //Ninguém precisa saber que deu erro
+                }
+            }
+        }
     }
 
     public void fecharConexao(){
-        db.close();
+        
         try{
-            System.out.println("Conexão fechada...");
+            oObjectContainer.close();
+            sb.append("Conexão fechada");
+        }catch(Exception ex){
+            sb.append("Erro em fechar conexão: "+ex);
         }
-        catch(Exception ex){
-            System.out.println("Erro em fechar conexão: "+ex);
+        finally{
+
+            if (sb.length() > 0) {
+                try {
+                    throw new Exception(sb.toString());
+                }
+                catch (Exception ex) {
+                    //Ninguém precisa saber que deu erro
+                }
+            }
         }
     }
 
     public ObjectContainer getDb()
     {
-        return db;
+        return oObjectContainer;
     }
 }
